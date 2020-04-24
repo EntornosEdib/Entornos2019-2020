11bce8a0402e8b3001b38e397a79df6a69713891
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-24 11:44:20 +0200

    Version 1.0 - Inicial

--- src/main/java/Pruebas.java
+++ src/main/java/Pruebas.java
@@ -9,6 +9,17 @@
  * @author Rafael
  * @version 1.0
  */
+import java.io.Console;
+import java.io.PrintWriter;
+
 public class Pruebas {
-    
+    public static void main(String[] args) {
+        Console console = System.console();
+        if(console != null) {
+            PrintWriter pw = console.writer();
+            pw.println("Console class writer() method example");
+        }else {
+            System.out.println("Console is null");
+        }
+    }
 }
